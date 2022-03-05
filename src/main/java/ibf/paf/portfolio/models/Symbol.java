dc70068e0fccaeb0323db7730ee68c233716aa7f
Chan Yew Yen <chan.yewyen@gmail.com>
2022-03-05 13:00:53 +0800

    Add Symbol model

---
+++ src/main/java/ibf/paf/portfolio/models/Symbol.java
@@ -0,0 +1,10 @@
+package ibf.paf.portfolio.models;
+
+public record Symbol(
+        String symbol,
+        String name,
+        String type,
+        String region,
+        String currency) {
+
+}
