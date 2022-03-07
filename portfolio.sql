e4df211dfc2777cdcc731956df59769b7b5ea366
Chan Yew Yen <chan.yewyen@gmail.com>
2022-03-08 07:06:11 +0800

    Add cascade deletes

--- portfolio.sql
+++ portfolio.sql
@@ -15,14 +15,14 @@ CREATE TABLE portfolio (
     p_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     p_name VARCHAR(64) NOT NULL,
     u_id INTEGER,
-    CONSTRAINT FK_UserPortfolio FOREIGN KEY(u_id) REFERENCES user(u_id)
+    CONSTRAINT FK_UserPortfolio FOREIGN KEY(u_id) REFERENCES user(u_id) ON DELETE CASCADE
 );
 SELECT "Creating table portfolio_stock" AS "";
 CREATE TABLE portfolio_stock (
     ps_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     symbol VARCHAR(64) NOT NULL,
     p_id INTEGER,
-    CONSTRAINT FK_PortfolioStock FOREIGN KEY(p_id) REFERENCES portfolio(p_id),
+    CONSTRAINT FK_PortfolioStock FOREIGN KEY(p_id) REFERENCES portfolio(p_id) ON DELETE CASCADE,
     UNIQUE KEY unique_portfolio_stock (p_id, symbol)
 );
 SELECT "Creating table portfolio_stock_transaction" AS "";
@@ -33,20 +33,20 @@ CREATE TABLE portfolio_stock_transaction (
     brokerage_fees FLOAT,
     tx_datetime DATETIME NOT NULL,
     ps_id INTEGER,
-    CONSTRAINT FK_PortfolioStockTransaction FOREIGN KEY(ps_id) REFERENCES portfolio_stock(ps_id)
+    CONSTRAINT FK_PortfolioStockTransaction FOREIGN KEY(ps_id) REFERENCES portfolio_stock(ps_id) ON DELETE CASCADE
 );
 SELECT "Creating table watchlist" AS "";
 CREATE TABLE watchlist (
     w_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     w_name VARCHAR(64) NOT NULL,
     u_id INTEGER,
-    CONSTRAINT FK_UserWatchlist FOREIGN KEY(u_id) REFERENCES user(u_id)
+    CONSTRAINT FK_UserWatchlist FOREIGN KEY(u_id) REFERENCES user(u_id) ON DELETE CASCADE
 );
 SELECT "Creating table watchlist_stock" AS "";
 CREATE TABLE watchlist_stock (
     ws_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     symbol VARCHAR(64) NOT NULL,
     w_id INTEGER,
-    CONSTRAINT FK_WatchlistStock FOREIGN KEY(w_id) REFERENCES watchlist(w_id),
+    CONSTRAINT FK_WatchlistStock FOREIGN KEY(w_id) REFERENCES watchlist(w_id) ON DELETE CASCADE,
     UNIQUE KEY unique_watchlist_stock (w_id, symbol)
 );
